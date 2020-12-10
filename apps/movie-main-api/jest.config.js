6d94688c50b3f77f40eaa0681fbeffc207d46a89
m-joseph27 <muh.joseph27@gmail.com>
2020-12-10 11:01:07 +0700

    initial commit

---
+++ apps/movie-main-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'movie-main-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/movie-main-api',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/movie-main-api'],
+};
